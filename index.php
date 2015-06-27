747597b716003a42df95bb205a04ebc4f221ee9c
James Hazard <james.g.hazard@gmail.com>
2015-06-27 18:23:28 +0200

    zmon code

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
+<?php 
 
-<?php include("ccmac_helpers.php"); ?>
+DEFINE('ROOT' ,dirname(__FILE__) );									// Root directory of site
+DEFINE('LIB_PATH', ROOT . '/vendor/CommonAccord/ccmac/library');								// Create global variable to our library	
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . LIB_PATH . DIRECTORY_SEPARATOR);	// Include it in PHP include/require search
+
+DEFINE('ASSETS_PATH', 'vendor/CommonAccord/ccmac/assets');									// Create global variable to our css and js assets	
+
+include("ccmac_helpers.php"); 
 
 
