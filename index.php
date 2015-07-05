3c2eba152692e2cbfefa48db6e2fed147dbe2c2b
James Hazard <james.g.hazard@gmail.com>
2015-07-06 00:05:11 +0200

    assets/

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ DEFINE('ROOT' ,dirname(__FILE__) ); // Root directory of site
 DEFINE('LIB_PATH', ROOT . '/vendor/CommonAccord/cmacc/library'); // Create global variable to our library	
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . LIB_PATH . DIRECTORY_SEPARATOR);	// Include it in PHP include/require search
 
-DEFINE('ASSETS_PATH', 'vendor/CommonAccord/assets');  // Create global variable to our css and js assets
+DEFINE('ASSETS_PATH', 'assets');  // Create global variable to our css and js assets
 
 DEFINE('URLFORDOCSINREPO', 'https://github.com/CommonAccord/Site-My/blob/master/Doc/');	// Use for customizing the link to a repo
 
